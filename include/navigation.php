ce16f88a79efd183f010a4fe84005e03b17d2084
Alex Mester <zjaakie@gmail.com>
2020-03-18 19:29:47 +0100

    added space

--- include/navigation.php
+++ include/navigation.php
@@ -12,7 +12,7 @@ $query = "SELECT DISTINCT * FROM menu";
 $sql = $pdo->prepare($query);
 $sql->execute() or die("Unable to execute query!");
 while ($row = $sql->fetch(PDO::FETCH_BOTH)) {
-    echo "<a class=\"nav-item nav-link\" href=\"" . $row['url'] . "\">" . $row['title'] . "</a>";
+    echo "<a class=\"nav-item nav-link\" href=\"" . $row['url'] . "\">" . $row['title'] . "</a> ";
 }
 
 ?>
