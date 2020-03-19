f5b721434fd25c282bef121cae0a46ea4a1bef98
Alex Mester <zjaakie@gmail.com>
2020-03-19 18:18:27 +0100

    navigation.php changes

--- include/navigation.php
+++ include/navigation.php
@@ -47,7 +47,18 @@ function sub_menu($array = array(), $parent_id = false) {
         echo "</ul>";
     }
 }
-
+function sub_sub_menu($array = array(), $parent_id = false) {
+    if(!empty($array[$parent_id])) {
+        echo '<ul class="dropdown-menu" aria-labelledby="dropdown2-1">' . PHP_EOL;
+        var_dump($array[$parent_id]);
+        foreach ($array[$parent_id] as $item) {
+            if ($item['is_dropdown'] == false) {
+                echo '<li class="dropdown-item"><a href="' . $item['href'] . '">' . $item['name'] . '</a></li>' . PHP_EOL;
+            }
+        }
+        echo "</ul>" . PHP_EOL;
+    }
+}
 
 menu_builder($pdo, 0)
 
