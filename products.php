27766381a6d522807beba150df625a487a1c4707
Alex <zjaakie@gmail.com>
2026-07-04 21:15:28 +0200

    Update products page to use MySQL backend

--- products.php
+++ products.php
@@ -1,117 +1,106 @@
 <?php
 require_once 'include/config.php';
 require_once 'include/session.php';
+require_once 'include/database.php';
 require_once 'include/header.php';
 require_once 'include/navigation.php';
 
+$product_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-?>
-    <div class="container">
-<?php
-if (isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])) {
-    $sql = $pdo->prepare("SELECT * FROM products WHERE id = :id");
-    $sql->bindParam(':id', $_GET['id']);
-    $sql->execute() or die("Unable to execute query!");
-        
-    if ($sql->rowCount() > 0) {
-        while ($row = $sql->fetch(PDO::FETCH_BOTH)) {
-            ?>
-            <div class="row">
-              <div class="sm-col-4">
-                <div class="card bg-light border-dark mb-3" style="overflow: hidden">
-            <?php
-           echo "            <div class=\"card-header\">" . $row['product'] . "</div>" . PHP_EOL;
-            if (empty($row['pictures'])) {
-                echo "      <img class=\"card-img-top\" src=\"img/empty.png\" alt=\"" . $row['product'] . "\">" . PHP_EOL;
+if ($product_id) {
+    // Single product view
+    $product = getProductById($product_id);
+    
+    if (!$product) {
+        header('Location: products.php');
+        exit();
+    }
+    
+    echo "<div class='container mt-5'>";
+    echo "  <div class='row'>";
+    echo "    <div class='col-md-4'>";
+    if ($product['image_url']) {
+        echo "      <img src='" . htmlspecialchars($product['image_url']) . "' class='img-fluid' alt='" . htmlspecialchars($product['product']) . "'>";
+    } else {
+        echo "      <div class='bg-light' style='height: 300px; display: flex; align-items: center; justify-content: center;'>";
+        echo "        <span class='text-muted'>No image available</span>";
+        echo "      </div>";
+    }
+    echo "    </div>";
+    
+    echo "    <div class='col-md-8'>";
+    echo "      <h2>" . htmlspecialchars($product['product']) . "</h2>";
+    echo "      <p class='text-muted'>" . htmlspecialchars($product['category']) . "</p>";
+    echo "      <h3>€ " . number_format($product['price'], 2, ',', '.') . "</h3>";
+    echo "      <p>" . nl2br(htmlspecialchars($product['description'])) . "</p>";
+    
+    if ($product['stock'] > 0) {
+        echo "      <form action='cart.php' method='GET'>";
+        echo "        <input type='hidden' name='id' value='" . $product['id'] . "'>";
+        echo "        <input type='hidden' name='action' value='add'>";
+        echo "        <div class='form-group'>";
+        echo "          <label>Quantity:</label>";
+        echo "          <input type='number' name='amount' value='1' min='1' max='" . $product['stock'] . "' class='form-control' style='width: 100px;'>";
+        echo "        </div>";
+        echo "        <button type='submit' class='btn btn-success btn-lg'>Add to Cart</button>";
+        echo "      </form>";
+        echo "      <p class='text-success mt-2'>" . $product['stock'] . " in stock</p>";
+    } else {
+        echo "      <p class='text-danger'><strong>Out of stock</strong></p>";
+    }
+    
+    echo "      <a href='products.php' class='btn btn-outline-secondary mt-3'>Back to Products</a>";
+    echo "    </div>";
+    echo "  </div>";
+    echo "</div>";
+} 
+else {
+    // Products listing
+    $products = getProducts();
+    
+    echo "<div class='container mt-5'>";
+    echo "  <h2>Our Products</h2>";
+    echo "  <div class='row mt-4'>";
+    
+    if (empty($products)) {
+        echo "    <div class='col-12'>";
+        echo "      <p>No products available.</p>";
+        echo "    </div>";
+    } else {
+        foreach ($products as $product) {
+            echo "    <div class='col-md-4 mb-4'>";
+            echo "      <div class='card h-100'>";
+            
+            if ($product['image_url']) {
+                echo "        <img src='" . htmlspecialchars($product['image_url']) . "' class='card-img-top' alt='" . htmlspecialchars($product['product']) . "'>";
+            } else {
+                echo "        <div class='card-img-top bg-light' style='height: 200px; display: flex; align-items: center; justify-content: center;'>";
+                echo "          <span class='text-muted'>No image</span>";
+                echo "        </div>";
             }
-            else {
-                ?>
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                  <ol class="carousel-indicators">
-                <?php
-                $pictures = unserialize($row['pictures']);
-                $counter = count($pictures);
-                $i = 0;
-                
-                while ($i < $counter) {
-                    if ($i == 0) {
-                        echo "        <li data-target=\"#carouselExampleIndicator\" data-slide-to=\"" . $i . "\" class=\"active\"></li>" . PHP_EOL;
-                        $i++;
-                    }
-                    else {
-                        echo "        <li data-target=\"#carouselExampleIndicator\" data-slide-to=\"" . $i . "\"></li>" . PHP_EOL;
-                        $i++;
-                    }
-                }
-                ?>
-                  </ol>
-                  <div class="carousel-inner">
-                <?php
-                $i = 0;
-                foreach ($pictures as $picture) {
-                    if ($i == 0) {
-                        echo "        <div class=\"carousel-item active\">" . PHP_EOL;
-                        echo "            <img class=\"d-block w-100\" src=\"" . $location . $picture . "\" alt=\"" . $row['product'] . "\">" . PHP_EOL;
-                        echo "        </div>" . PHP_EOL;
-                        $i++;
-                    }
-                    else {
-                        echo "        <div class=\"carousel-item\">" . PHP_EOL;
-                        echo "            <img class=\"d-block w-100\" src=\"" . $location . $picture . "\" alt=\"" . $row['product'] . "\">" . PHP_EOL;
-                        echo "        </div>" . PHP_EOL;
-                    }
-                }
-                ?>
-                  </div>
-                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>
-        <?php
+            
+            echo "        <div class='card-body'>";
+            echo "          <h5 class='card-title'><a href='products.php?id=" . $product['id'] . "'>" . htmlspecialchars($product['product']) . "</a></h5>";
+            echo "          <p class='card-text'>" . htmlspecialchars(substr($product['description'], 0, 100)) . "...</p>";
+            echo "          <p class='text-muted'>" . htmlspecialchars($product['category']) . "</p>";
+            echo "          <h5>€ " . number_format($product['price'], 2, ',', '.') . "</h5>";
+            
+            if ($product['stock'] > 0) {
+                echo "          <a href='products.php?id=" . $product['id'] . "' class='btn btn-primary'>View Details</a>";
+                echo "          <a href='cart.php?id=" . $product['id'] . "&action=add' class='btn btn-success'>Add to Cart</a>";
+            } else {
+                echo "          <button class='btn btn-secondary' disabled>Out of Stock</button>";
             }
-            echo "            <div class=\"card-header\">" . $row['product'] . "</div>" . PHP_EOL;
-            echo "              <div class=\"card-body\">" . PHP_EOL;
-            echo "                <p class=\"card-text\">" . PHP_EOL;
-            echo "                  <span>" . nl2br($row['description']) . "</span><br>" . PHP_EOL;
-            echo "                  <span>&euro; " . number_format($row['price'], 2, ',', '.') . "</span><br>" . PHP_EOL;
-            echo "                  <span>Stock: " . $row['stock'] . "</span><br>" . PHP_EOL;
-            echo "                </p>\n";
-            echo "                <a href=\"cart.php?action=add&id=" . $row['id'] . "\" class=\"btn btn-primary\">Buy</a>\n";
-            if (isset($_SESSION['admin']) && $_SESSION['admin'] == "1") {
-                echo "            <a href=\"admin/action.php?action=modify&id=" . $row['id'] . "\" class=\"btn btn-warning pull-xs-right\">Modify</a>\n";
-                echo "            <a href=\"admin/action.php?action=remove&id=" . $row['id'] . "\" class=\"btn btn-danger pull-xs-right\">Remove</a>\n";
-            }
-            echo "              </div>" . PHP_EOL;
-            echo "            </div>" . PHP_EOL;
-            echo "          </div>\n";
-            echo "      </div>\n";
-            echo "  </div>\n";
-            echo "</div>\n";
-
-            ?>
-                </div>
-              </div>
-            </div>
-        <?php
+            
+            echo "        </div>";
+            echo "      </div>";
+            echo "    </div>";
         }
     }
-    else {
-        echo "    <div class=\"alert alert-info\" role=\"alert\">" . PHP_EOL;
-        echo "        This product doesn't exist!" . PHP_EOL;
-        echo "    </div>" . PHP_EOL;
-    }
+    
+    echo "  </div>";
+    echo "</div>";
 }
-else {
-    echo "    <div class=\"alert alert-info\" role=\"alert\">" . PHP_EOL;
-    echo "        This product doesn't exist!" . PHP_EOL;
-    echo "    </div>" . PHP_EOL;
-}
-?>
-    </div>
-<?php
+
 require_once 'include/footer.php';
+?>
